239f62125c044440f489b17d7036b0a90b4609e4
AMKolev22 <amkolev22@codingburgas.bg>
2024-10-25 20:54:42 +0300

    dsajdjsadsa

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import auth from "./routes/auth"; 
+import cors from 'cors';
+
+import auth from "./routes/auth";
 import deposit from "./routes/deposit";
 import withdraw from "./routes/withdraw";
 import send from "./routes/send";
@@ -9,21 +11,28 @@ import token from "./routes/session";
 import deleteTransaction from "./routes/deleteTransaction";
 import getTokenInfo from "./routes/getSessionInfo";
 import deleteToken from "./routes/deleteSession";
-const app: Application = express();
 
+const app: Application = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
 
+// Prisma client with detailed logging
 export const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
+});
+
+app.use((req: Request, res: Response, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
 });
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send({ data: 'ROOT' });
 });
 
-app.use('/auth', auth); 
+app.use('/auth', auth);
 app.use('/deposit', deposit);
 app.use('/withdraw', withdraw);
 app.use('/send', send);
