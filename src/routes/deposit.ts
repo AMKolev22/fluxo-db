7e5535802db93cc1bf738a7046c375333e854269
unknown <vooleksandrov22@codingburgas.bg>
2024-11-04 23:38:08 +0200

    Update withdraw and deposit, so that transactions with budgets are available

--- src/routes/deposit.ts
+++ src/routes/deposit.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
     try {
-        const { amount, category, email, target } = req.body;
+        const { amount, category, email, target, type } = req.body;
         const user = await prisma.user.findUnique({
             where: { email },
         });
@@ -17,25 +17,68 @@ router.post("/", async (req: Request, res: Response) => {
         const hours = date.getHours().toString().padStart(2, '0');
         const minutes = date.getMinutes().toString().padStart(2, '0'); 
         const currentTime = `${hours}:${minutes}`;
-        const transaction = await prisma.transaction.create({
-            data: {
-                amount: amount,
-                category: category,
-                target: target,
-                userId: user.id,
-                timeProcessed: currentTime,
-                typeOfTransaction: "DEPOSIT"
-            },
-        });
+        
+        const amountFloat = parseFloat(amount);
 
-        const updatedUser = await prisma.user.update({
-            where: { id: user.id },
-            data: {
-                balance: user.balance + amount,
-            },
-        });
+        if (type == "DEPOSIT"){
+            const transaction = await prisma.transaction.create({
+                data: {
+                    amount: amount,
+                    category: category,
+                    target: target,
+                    userId: user.id,
+                    timeProcessed: currentTime,
+                    typeOfTransaction: "DEPOSIT"
+                },
+            });
 
-        return res.status(201).json({ transaction, updatedUser });
+            const updatedUser = await prisma.user.update({
+                where: { id: user.id },
+                data: {
+                    balance: user.balance + amount,
+                },
+            });
+
+            return res.status(201).json({ transaction, updatedUser });
+        } else{
+            const budgetId = type.split(":")[2];
+            const budget = await prisma.budget.findUnique({
+                where: {id: budgetId},
+            });
+
+            if (budget == null){throw new Error("Budget is null");}
+
+            if (user.balance >= amountFloat){
+
+                const transaction = await prisma.transaction.create({
+                    data: {
+                        amount: amountFloat,
+                        category: category,
+                        target: budget.title,
+                        userId: user.id,
+                        timeProcessed: currentTime,
+                        typeOfTransaction: type,
+                    },
+                });
+
+                const updatedUser = await prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                        balance: user.balance - amountFloat,
+                    },
+                });
+
+                const updatedBudget = await prisma.budget.update({
+                    where: {id: budget.id},
+                    data:{
+                        amountInserted: budget.amountInserted + amountFloat,
+                    },
+                });
+
+                return res.status(201).json({ transaction, updatedUser, updatedBudget });
+            }
+        }
+        
     } catch (error) {
         console.error(error);
         return res.status(500).send({ error: 'Internal server error' });
--- src/routes/withdraw.ts
+++ src/routes/withdraw.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
     try {
-        const { amount, category, email, target } = req.body;
+        const { amount, category, email, target, type } = req.body;
         const user = await prisma.user.findUnique({
             where: { email },
         });
@@ -13,34 +13,83 @@ router.post("/", async (req: Request, res: Response) => {
         if (!user) {
             return res.status(404).send({ error: 'User not found' });
         }
-        if (amount <= user.balance){
         const date = new Date();
         const hours = date.getHours().toString().padStart(2, '0');
-        const minutes = date.getMinutes().toString().padStart(2, '0'); 
+        const minutes = date.getMinutes().toString().padStart(2, '0');
         const currentTime = `${hours}:${minutes}`;
-        const transaction = await prisma.transaction.create({
-            data: {
-                amount: amount,
-                category: category,
-                target: target,
-                userId: user.id,
-                timeProcessed: currentTime,
-                typeOfTransaction: "WITHDRAW"
-            },
-        });
 
-        const updatedUser = await prisma.user.update({
-            where: { id: user.id },
-            data: {
-                balance: user.balance - amount,
-            },
-        });
+        const amountFloat = parseFloat(amount);
+        
+        if (type == "WITHDRAW"){
 
-        return res.status(201).json({ transaction, updatedUser });
-    }
-    else{
-        throw new Error("Insufficient funds");
-    }
+            if (amount <= user.balance){
+                    
+                const transaction = await prisma.transaction.create({
+                    data: {
+                        amount: amountFloat,
+                        category: category,
+                        target: target,
+                        userId: user.id,
+                        timeProcessed: currentTime,
+                        typeOfTransaction: "WITHDRAW"
+                    },
+                });
+
+                const updatedUser = await prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                        balance: user.balance - amountFloat,
+                    },
+                });
+
+                return res.status(201).json({ transaction, updatedUser });
+            }
+            else{
+                throw new Error("Insufficient funds");
+            }
+
+        } else{
+            const budgetId = type.split(":")[2];
+            const budget = await prisma.budget.findUnique({
+                where: {id: budgetId},
+            });
+
+            if (budget == null){throw new Error("Budget is null");}
+
+            if (budget.amountInserted >= amountFloat){
+
+                const transaction = await prisma.transaction.create({
+                    data: {
+                        amount: amountFloat,
+                        category: category,
+                        target: budget.title,
+                        userId: user.id,
+                        timeProcessed: currentTime,
+                        typeOfTransaction: type,
+                    },
+                });
+
+                const updatedUser = await prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                        balance: user.balance + amountFloat,
+                    },
+                });
+
+                const updatedBudget = await prisma.budget.update({
+                    where: {id: budget.id},
+                    data:{
+                        amountInserted: budget.amountInserted - amountFloat,
+                    },
+                });
+
+                return res.status(201).json({ transaction, updatedUser, updatedBudget });
+
+            } else{
+                throw new Error("Insufficient funds");
+            }
+        }
+        
     } catch (error) {
         console.error(error);
         return res.status(500).send({ error: 'Internal server error' });
